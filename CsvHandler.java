b7918aa40a1fef580b49c94e33adff4cf4a92fc2
souremillime <emil.deluca@icloud.com>
2025-08-04 17:59:01 +0200

    aggiunto metodo letturaConsecutiva() e ridefinito l'uso del reader come globale, non per singoli metodi

--- CsvHandler.java
+++ CsvHandler.java
@@ -5,10 +5,52 @@ import java.io.IOException;
 
 public class CsvHandler{
     String csvPath;
-    int numCol;
+    int numCol, numLines;
+    BufferedReader reader;
+    FileWriter writer;
+
+//inizializazione oggetto
 
     //costruttore
     public CsvHandler(String csvPath) {
+        this.csvPath = csvPath;
+        try{
+            reader = new BufferedReader(new FileReader(csvPath));
+            definisciDimensioni();
+            if(this.numCol == 1){
+                System.out.println("file inizializzato");//debug
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //definisce il numero di colonne e il numero di righe che ha un file CSV
+    private void definisciDimensioni() throws IOException{
+        String csvString = reader.readLine();
+        int count = 0;
+        for (int i = 0; i < csvString.length(); i++) {
+            if (csvString.charAt(i) == ',') {
+                count++;
+            } else if (csvString.charAt(i) == '"') {
+                while (csvString.charAt(i++) != '"');
+            }
+        }
+        this.numCol = count+1;
+        count = 1;
+
+        while(reader.readLine() != null){
+            count++;
+        }
+        this.numLines = count;
+    //debug
+        System.out.println("numCol: " + numCol);
+        System.out.println("numLine: " + numLines);
+
+    }
+/*
+    public void loadFile(String csvPath){
         this.csvPath = csvPath;
         try(BufferedReader reader = new BufferedReader(new FileReader(csvPath))){
             definisciColonne(reader.readLine());
@@ -19,12 +61,14 @@ public class CsvHandler{
         }catch(IOException e){
             e.printStackTrace();
         }
-    }
 
-    //viene letta la prima riga
-    public String[] getFirstRow(){
-        String[] outStrings = new String[numCol-1];
-        try(BufferedReader reader = new BufferedReader(new FileReader(csvPath))){
+    }*/ //debug
+
+    //viene letta la prima riga e resettato il reader
+    public String[] getFirstLine(){
+        String[] outStrings = new String[numCol];
+        try{
+            reader.reset();
             outStrings = csvToString(reader.readLine());
 
         }catch(IOException e){
@@ -32,13 +76,10 @@ public class CsvHandler{
         }
         return outStrings;
     }
-
-//metodi privati
-
     /*lettura*/
     //conversione da codifica CSV a un array di stringe contenenti le informazioni delle singole colonne
-    private String[] csvToString(String csvString){
-        int columnNum = 0, lastComma = -1; //inizia da -1 visto che nel substring viene sommato 1, questo per fare laggere il brimo elemento dalla riga 0
+    public String[] csvToString(String csvString){
+        int columnNum = 0, lastComma = -1; //inizia da -1 visto che nel substring viene sommato 1, questo per fare laggere il primo elemento dalla riga 0
         String outString[] = new String[numCol];
         
         for(int i = 0; i< (csvString.length()); i++){
@@ -73,38 +114,32 @@ public class CsvHandler{
         return outString;
     }
 
-    //definisce il numero di colonne che ha un file CSV
-    private void definisciColonne(String csvString){
-        int count = 0;
-        for (int i = 0; i < csvString.length(); i++) {
-            if (csvString.charAt(i) == ',') {
-                count++;
-            } else if (csvString.charAt(i) == '"') {
-                while (csvString.charAt(i++) != '"');
-            }
-        }
-        this.numCol = count+1;
-
-        System.out.println("numCol: " + numCol);
-
+    //legge in continuazione dalla riga precedentemente letta
+    public String[] letturaConsecutiva() throws IOException{
+        return csvToString(reader.readLine());
     }
 
     /*scrittura*/
 
     //scrittura di una riga CSV
-    void scriviLineaCSV(String linea[]){
-        if (linea.length > numCol){
+    void nuovaRigaCSV(String riga[]){
+        if (riga.length > numCol){
             System.err.println("oggetto troppo lungo");//debug
         }else{
-            try(FileWriter writer = new FileWriter(csvPath)){
-                for(int i = 0; i<linea.length;i++ ){
-                    writer.write(linea[i] + ",");
+            try(FileWriter writer = new FileWriter(csvPath, true)){
+                writer.write("\n");
+                for(int i = 0; i<riga.length-1;i++ ){
+                    writer.write(riga[i] + ",");
                 }
-                writer.write(linea[numCol-1]);
+                writer.write(riga[numCol-1]);
             }catch(IOException e){
                 e.printStackTrace();
             }
         }
+
+    }
+
+    void riscriviRigaCSV(String riga[]){
 
     }
 
