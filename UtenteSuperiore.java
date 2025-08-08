64328e5fe5acea5c5197486d3a831e72d1140439
souremillime <emil.deluca@icloud.com>
2025-08-08 19:48:44 +0200

    aggiunta gestione utenti

---
+++ UtenteSuperiore.java
@@ -0,0 +1,12 @@
+public class UtenteSuperiore extends Utente{
+    
+    public static enum MENU {STANDARD};
+
+    public UtenteSuperiore(String username) {
+        super(username);
+    }
+
+    
+
+
+}
